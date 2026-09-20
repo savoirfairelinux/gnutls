91e56cfc285a4e56dec91b014b97d252c140daac
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2026-09-19 23:32:45 -0400

    project: Update for 3.8.13 merge

--- SMP/version.h
+++ SMP/version.h
@@ -1,5 +1,5 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.8.8"
+#define PACKAGE_VERSION "3.8.13"
 
 /* Version number of package */
-#define VERSION "3.8.8"
+#define VERSION "3.8.13"
