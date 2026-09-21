37840cfa523b5fcf3f4ca354741ea0190e613a98
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2026-09-21 15:25:37 -0400

    smp: define _NETTLE_ALIGN16 and stdalign guards at top of config.h

--- SMP/config.h
+++ SMP/config.h
@@ -1,3 +1,27 @@
+#if defined(_MSC_VER) && !defined(__cplusplus)
+# ifndef alignas
+#  define alignas(a) __declspec(align(a))
+# endif
+# ifndef alignof
+#  define alignof(t) __alignof(t)
+# endif
+# ifndef _INC_STDALIGN
+#  define _INC_STDALIGN
+# endif
+# ifndef __STDALIGN_H
+#  define __STDALIGN_H
+# endif
+# ifndef __alignas_is_defined
+#  define __alignas_is_defined 1
+# endif
+# ifndef __alignof_is_defined
+#  define __alignof_is_defined 1
+# endif
+# ifndef _NETTLE_ALIGN16
+#  define _NETTLE_ALIGN16 __declspec(align(16))
+# endif
+#endif
+
 /* Define if building universal (internal helper macro) */
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
 
--- SMP/gnulib/lib/stdalign.h
+++ SMP/gnulib/lib/stdalign.h
@@ -11,11 +11,21 @@
 # endif
 #endif
 
+#ifndef _INC_STDALIGN
+# define _INC_STDALIGN
+#endif
+#ifndef __STDALIGN_H
+# define __STDALIGN_H
+#endif
 #ifndef __alignas_is_defined
 # define __alignas_is_defined 1
 #endif
 #ifndef __alignof_is_defined
 # define __alignof_is_defined 1
+#endif
+
+#ifndef _NETTLE_ALIGN16
+# define _NETTLE_ALIGN16 __declspec(align(16))
 #endif
 
 #endif /* _SMP_STDALIGN_H */
